5638b62f85938b55873168d2734e95499fbce300
McLoyf <alex.ortiz242005@gmail.com>
2025-10-10 23:13:43 -0500

    Change database thing
    I need this to work

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const db = mysql.createConnection({
   host: process.env.MYSQLHOST || "localhost",
   user: process.env.MYSQLUSER || "root",
   password: process.env.MYSQLPASSWORD || "password",
-  database: process.env.MYSQLDATABASE || "railway",
+  database: process.env.MYSQLDATABASE || "web_game",
   port: process.env.MYSQLPORT || 3306,
 });
 
