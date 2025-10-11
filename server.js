1ae3be5886fbc653ba494323e87c8d126c9716c3
McLoyf <alex.ortiz242005@gmail.com>
2025-10-10 23:26:14 -0500

    Please work

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const db = mysql.createConnection({
   host: process.env.MYSQLHOST || "localhost",
   user: process.env.MYSQLUSER || "root",
   password: process.env.MYSQLPASSWORD || "password",
-  database: "web_game",
+  database: process.env.MYSQLDATABASE || "web_game",
   port: process.env.MYSQLPORT || 3306,
 });
 
