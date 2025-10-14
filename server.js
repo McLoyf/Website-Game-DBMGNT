1d9aa51bd254ec133112d2dec11540099a8e1111
McLoyf <alex.ortiz242005@gmail.com>
2025-10-13 22:26:23 -0500

    Added mysqldb name to get this to work

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  database: process.env.MYSQLDATABASE || "railway",
   port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
