995a6365b139efb906db41db114ba05aa016426a
Mcloyf <alexortiz242005@gmail.com>
2025-11-06 16:14:06 -0600

    Change railway path
    Remove the 'https://'

--- scripts/register.js
+++ scripts/register.js
@@ -1,5 +1,5 @@
 // scripts/register.js
-const BACKEND_URL = "https://website-game-dbmgnt-production.up.railway.app";
+const BACKEND_URL = "website-game-dbmgnt-production.up.railway.app";
 
 document.addEventListener("DOMContentLoaded", () => {
   const registerForm = document.getElementById("registerForm");
